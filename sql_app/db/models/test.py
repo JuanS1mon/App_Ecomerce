94e2e1b9adedafad45329ab799e00e9d6f931dd0
JuanSi <fjuansimon@gmail.com>
2025-03-31 01:03:55 -0300

    prueba de subida

--- sql_app/db/models/test.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, String, Boolean, Float
-from ..database import Base
-
-class Test(Base):
-    __tablename__ = 'test'
-
-    id = Column(Integer, primary_key=True, index=True, default=0)
-    campo1 = Column(String(50), default=' ')
--- sql_app/db/schemas/modulos.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel # Importamos BaseModel de pydantic para crear modelos de datos que se utilizarán para validar la entrada de datos y convertir los datos en diferentes formatos.
-
-class Modulos(BaseModel):
-    descripcion: str 
-    ejecutable: str
-    class Config:
-        from_attributes = True
